9aa20ac473a6419d9c37feddea80faa2c9fa9662
niemand108 <santilozano@gmail.com>
2019-08-06 19:21:11 +0200

    improved Earthquake

--- Earthquake/app/src/main/java/com/example/earthquake/EarthquakeRecyclerViewAdapter.kt
+++ Earthquake/app/src/main/java/com/example/earthquake/EarthquakeRecyclerViewAdapter.kt
@@ -5,10 +5,18 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import java.text.DecimalFormat
+import java.text.SimpleDateFormat
+import java.util.*
 
 
 class EarthquakeRecyclerViewAdapter(var mEarthQuakes: List<Earthquake>) :
     RecyclerView.Adapter<EarthquakeRecyclerViewAdapter.ViewHolder>() {
+    companion object {
+        var TIME_FORMAT = SimpleDateFormat("HH:mm", Locale.US)
+        var MAGNITUDE_FORMAT = DecimalFormat("0.0")
+    }
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         var v:View = LayoutInflater.from(parent.context)
             .inflate(R.layout.list_item_earthquake, parent, false)
@@ -20,22 +28,21 @@ class EarthquakeRecyclerViewAdapter(var mEarthQuakes: List<Earthquake>) :
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        holder.earquake = mEarthQuakes[position]
-        holder.detailsView.text = mEarthQuakes[position].toString()
+        var earthquake : Earthquake = mEarthQuakes.get(position)
+        holder.date.text = TIME_FORMAT.format(earthquake.date)
+        holder.details.text = earthquake.details
+        holder.magnitude.text = MAGNITUDE_FORMAT.format(earthquake.magnitude)
     }
 
     inner class ViewHolder:RecyclerView.ViewHolder {
-        var parentView:View
-        var detailsView:TextView
-        lateinit var earquake:Earthquake
+        var date:TextView
+        var details:TextView
+        var magnitude:TextView
 
         constructor(v: View) : super(v) {
-            parentView = v
-            detailsView = v.findViewById(R.id.list_item_earthquake_details)
-        }
-
-        override fun toString(): String {
-            return super.toString() + "'" + detailsView.text + "'"
+            date = v.findViewById<TextView>(R.id.date)
+            details = v.findViewById<TextView>(R.id.details)
+            magnitude = v.findViewById<TextView>(R.id.magnitude)
         }
     }
 }
