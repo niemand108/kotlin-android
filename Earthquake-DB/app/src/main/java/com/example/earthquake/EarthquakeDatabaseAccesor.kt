c19d80a643f19e3e87ff175763bc608467d91fe2
niemand108 <santilozano@gmail.com>
2019-08-09 13:42:20 +0200

    lock

--- Earthquake-DB/app/src/main/java/com/example/earthquake/EarthquakeDatabaseAccesor.kt
+++ Earthquake-DB/app/src/main/java/com/example/earthquake/EarthquakeDatabaseAccesor.kt
@@ -6,13 +6,15 @@ import androidx.room.Room
 class EarthquakeDatabaseAccesor {
     companion object {
         private var EarthquakeDatabaseInstance: EarthquakeDatabase? = null
-
+        private val LOCK = Any()
         private const val EARTHQUAKE_DB_NAME = "earthquake_db"
 
         fun getInstance(context: Context): EarthquakeDatabase {
             EarthquakeDatabaseInstance?.let { return it } ?: run {
-                EarthquakeDatabaseInstance =
-                    Room.databaseBuilder(context, EarthquakeDatabase::class.java, EARTHQUAKE_DB_NAME).build()
+                synchronized(LOCK) {
+                    EarthquakeDatabaseInstance =
+                        Room.databaseBuilder(context, EarthquakeDatabase::class.java, EARTHQUAKE_DB_NAME).build()
+                }
             }
             return EarthquakeDatabaseInstance!!
         }
